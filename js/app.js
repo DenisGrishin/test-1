3d559496aa34868e112671773cbf768477cb999a
Denis <666trall666@gmail.com>
2024-10-08 22:12:31 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4,27 +4,27 @@
   setTimeout(() => {
     if (e) {
       let e = new Event("windowScroll");
-      window.addEventListener("scroll", function (n) {
+      window.addEventListener("scroll", function (t) {
         document.dispatchEvent(e);
       });
     }
   }, 0),
     addEventListener("DOMContentLoaded", (e) => {
-      const n = (e = 0) => {
-        const n = document.querySelectorAll(".slider__item");
-        0 !== n.length &&
-          (n[e].classList.add("_current"),
-          e - 1 < 0 || n[e - 1].classList.remove("_current"));
+      const t = (e = 0) => {
+        const t = document.querySelectorAll(".slider__item");
+        0 !== t.length &&
+          (t[e].classList.add("_current"),
+          e - 1 < 0 || t[e - 1].classList.remove("_current"));
       };
       (() => {
         const e = document.getElementById("currentDate"),
-          n = new Date();
-        let t = n.getDate(),
-          o = n.getMonth(),
-          r = n.getDay();
-        t < 10 && (t = "0" + t),
-          (e.children[0].innerHTML = t),
-          (e.children[1].innerHTML = `${["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"][o]},`),
+          t = new Date();
+        let n = t.getDate(),
+          r = t.getMonth(),
+          i = t.getDay();
+        n < 10 && (n = "0" + n),
+          (e.children[0].innerHTML = n),
+          (e.children[1].innerHTML = `${["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"][r]},`),
           (e.children[2].innerHTML = [
             "Понедельник",
             "Вторник",
@@ -33,46 +33,45 @@
             "Пятница",
             "Суббота",
             "Воскресенье",
-          ][r]);
+          ][i]);
       })(),
         (() => {
           let e;
-          function t() {
+          function n() {
             let e = document.getElementById("clock");
             if (e) {
-              let t = new Date(),
-                o = t.getHours();
-              o < 10 && (o = "0" + o), (e.children[0].innerHTML = o);
-              let r = t.getMinutes();
-              r < 10 && (r = "0" + r), (e.children[1].innerHTML = r);
-              let i = t.getSeconds();
+              let n = new Date(),
+                r = n.getHours();
+              r < 10 && (r = "0" + r), (e.children[0].innerHTML = r);
+              let i = n.getMinutes();
+              i < 10 && (i = "0" + i), (e.children[1].innerHTML = i);
+              let o = n.getSeconds();
               if (
-                (i < 10 && (i = "0" + i),
-                (e.children[2].innerHTML = i),
-                Number(o) >= 6 && Number(o) < 12)
+                (o < 10 && (o = "0" + o),
+                (e.children[2].innerHTML = o),
+                Number(r) >= 6 && Number(r) < 12)
               )
-                return void n(1);
-              if (Number(o) >= 12 && Number(o) < 18) return void n(2);
-              if (Number(o) >= 18 && Number(o) > 0) return void n(3);
-              if (Number(o) < 6 && Number(o) >= 0) return void n(0);
+                return void t(1);
+              if (Number(r) >= 12 && Number(r) < 18) return void t(2);
+              if (Number(r) >= 18 && Number(r) > 0) return void t(3);
+              if (Number(r) < 6 && Number(r) >= 0) return void t(0);
             }
           }
-          e || (e = setInterval(t, 1e3)), t();
+          e || (e = setInterval(n, 1e3)), n();
         })(),
-        navigator.geolocation,
-        console.log(navigator.geolocation);
+        navigator.geolocation.getCurrentPosition(function (e) {});
     }),
     (window.FLS = !0),
     (function (e) {
-      let n = new Image();
-      (n.onload = n.onerror =
+      let t = new Image();
+      (t.onload = t.onerror =
         function () {
-          e(2 == n.height);
+          e(2 == t.height);
         }),
-        (n.src =
+        (t.src =
           "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA");
     })(function (e) {
-      let n = !0 === e ? "webp" : "no-webp";
-      document.documentElement.classList.add(n);
+      let t = !0 === e ? "webp" : "no-webp";
+      document.documentElement.classList.add(t);
     });
 })();
